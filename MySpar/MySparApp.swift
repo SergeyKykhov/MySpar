9071bad71de909387a71eb8dd521de19029566e2
Sergey Kykhov <ghrle@mail.ru>
2023-08-16 11:58:41 +0300

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Sergey Kykhov on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
